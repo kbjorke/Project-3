9f725c4c22cf2d8d67751974e26d2cbdbaa297cf
Kristian Bjørke <kbjorke@gmail.com>
2013-10-25 17:47:26 +0200

    Made header file and some modifications to the source file.

--- RungeKutta4.cpp
+++ RungeKutta4.cpp
@@ -4,19 +4,15 @@
 using namespace std;
 using namespace arma;
 
-int RK4 ( double h, double t, mat *u,
-          mat (*func)(double t, mat *u) )
+void RK4 ( double h, double t, mat *u,
+         mat (*func)(double t, mat u) )
 {
         static mat *k1, *k2, *k3, *k4;
-        static mat temp1, temp2, temp3;
 
-        *k1 = h*func(t, u);
-        temp1 = (*u + *k1/2);
-        *k2 = h*func(t + h/2, &temp1);
-        temp2 = (*u + *k2/2);
-        *k3 = h*func(t + h/2, &temp2);
-        temp3 = (*u + *k3);
-        *k4 = h*func(t + h, &temp3);
+        *k1 = h*func(t, *u);
+        *k2 = h*func(t + h/2, (*u + *k1/2));
+        *k3 = h*func(t + h/2, (*u + *k2/2));
+        *k4 = h*func(t + h, (*u + *k3));
 
-        return 0;
+        *u = *u + (1./6)*(*k1 + *k2*2 + *k3*2 + *k4);
 }

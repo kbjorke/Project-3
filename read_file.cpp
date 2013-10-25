157d33097728f7467bf8fc77c9ed7686878c5dbd
Kristian Bjørke <kbjorke@gmail.com>
2013-10-25 14:45:09 +0200

    Made function for the reading of data from file.

---
+++ read_file.cpp
@@ -0,0 +1,60 @@
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include "read_file.h"
+#include "celestial_body.h"
+
+using namespace std;
+
+void read_file(char* filename, Celestial_Body **celest_bodies)
+{
+    char ID[20], dummy[20], pos[20], vel[20];
+    char *p_value, *v_value;
+    int dimension, objects, planet_nr, i;
+    double mass;
+
+
+    fstream input_file;
+    input_file.open(filename, ios::in);
+
+    input_file >> dummy >> dimension >> dummy >> objects;
+
+
+    double position[dimension], velocity[dimension];
+
+    *celest_bodies = new Celestial_Body[objects];
+
+    input_file >> dummy >> dummy >> dummy >> dummy;
+
+    planet_nr = 0;
+    while( true )
+    {
+        input_file >> ID >> mass >> pos >> vel;
+
+        if( input_file.eof() ) break;
+
+        p_value = strtok( pos, "(,)" );
+        position[0] = atof(p_value);
+
+        for ( i = 1; i < dimension; i++ )
+        {
+            p_value = strtok( NULL, "(,)" );
+            position[i] = atof(p_value);
+        }
+        v_value = strtok( vel, "(,)" );
+        velocity[0] = atof(v_value);
+        for ( i = 1; i < dimension; i++ )
+        {
+            v_value = strtok( NULL, "(,)" );
+            velocity[i] = atof(v_value);
+        }
+
+        celest_bodies[0][planet_nr] = Celestial_Body(ID, mass, dimension,
+                                                     position, velocity);
+        planet_nr++;
+    }
+
+    input_file.close();
+
+}
---
+++ read_file.h
@@ -0,0 +1,8 @@
+#ifndef READ_FILE_H
+#define READ_FILE_H
+
+#include "celestial_body.h"
+
+void read_file(char* filename, Celestial_Body **celest_bodies);
+
+#endif // READ_FILE_H

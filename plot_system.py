ca5d5df5cbbbc86cc884e6b9d2a6eab7cce3b3cd
Kristian Bjørke <kbjorke@gmail.com>
2013-10-29 15:57:38 +0100

    Completed temporary version of plotter.

--- plot_system.py
+++ plot_system.py
@@ -3,28 +3,52 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # filename = sys.argv[-1]
-filename = "output_EarthSunSystem.txt"
+filename = "../Project3-build/output_EarthJupiterSunSystem.txt"
 
 input_file = open(filename, 'r')
 
 i = -2
 for line in input_file:
     column = line.split()
+    
     if i == -2:
-        objects = int(column[1])
+        object_nr = int(column[1])
         dimensions = int(column[3])
         time_step = float(column[6])
         end_time = float(column[9])
 
         steps = int(np.ceil(end_time/time_step))+1
         
-        positions = np.zeros([objects, steps])
-        velocity = np.zeros([
+        positions = np.zeros([object_nr, dimensions, steps])
+        time = np.zeros(steps)
+        objects = []
 
-        i += 1
+    if i == -1:
+        for j in range(object_nr):
+            objects.append(column[j+1])
+
+    if i >= 0:
+        time[i] = float(column[0])
+        for j in range(object_nr):
+            coordinate = column[j+1][1:-1].split(",")
+            for k in range(dimensions):
+                positions[j][k][i] = float(coordinate[k])
+
+    i += 1
 
 
 
 input_file.close()
 
-print objects, dimensions, time_step, end_time, steps, i
+if dimensions == 2:
+
+    plt.figure(1)
+    for j in range(object_nr):
+        if objects[j] == "Sun":
+            plt.plot(positions[j][0], positions[j][1], 'or')
+        else:
+            plt.plot(positions[j][0], positions[j][1])
+        plt.hold('on')
+
+    plt.axis("equal")
+    plt.show()

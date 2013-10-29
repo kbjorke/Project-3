5df7670786df7a0b8d45bd49fe766598cfa3b327
Kristian Bjørke <kbjorke@gmail.com>
2013-10-29 14:50:20 +0100

    Made interface with keyword argument to give end time and time step.

--- main.cpp
+++ main.cpp
@@ -11,14 +11,32 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    int objects;
+    int objects, dimension, i;
+    double end_time, time_step;
+    char *input_file, output_file[40], buffer[40];
+
+    input_file = argv[argc-1];
+
+    for( i = 0; i < argc-1; i++ ){
+        if((string(argv[i]).find("-") == 0 && string(argv[i]).find("time")!=string::npos)){
+            end_time = atof(argv[i+1]);
+        }
+        if((string(argv[i]).find("-") == 0 && string(argv[i]).find("h")!=string::npos)){
+            time_step = atof(argv[i+1]);
+        }
+    }
+
+    strcpy(buffer, input_file);
+    strcat(output_file, "output_");
+    strcat(output_file, strtok(buffer, "."));
+    strcat(output_file, ".txt");
 
     Celestial_Body *celest_bodies;
 
-    read_file(argv[1], &celest_bodies, &objects);
+    read_file(input_file, &celest_bodies, &objects, &dimension);
 
-    Planetary_System planetary_system(celest_bodies, objects);
+    Planetary_System planetary_system(celest_bodies, objects, dimension);
 
-    planetary_system.evolve(10,3);
+    planetary_system.evolve(output_file, end_time, time_step);
 }
 
